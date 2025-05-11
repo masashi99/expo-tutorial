dc2f476d1609f2bea4ef9e4e88974038345c8d59
masashi99 <sgmdq601@gmail.com>
2025-05-11 22:55:11 +0900

    add

---
+++ components/ImageViewer.tsx
@@ -0,0 +1,20 @@
+import { Image, ImageSource } from "expo-image"
+
+type Props = {
+  imgSource: ImageSource
+  selectedImage?: string;
+}
+
+export default function ImageViewer({imgSource, selectedImage}: Props) {
+  const imageSource = selectedImage ? {uri: selectedImage} : imgSource
+  return <Image source={imageSource} style={style.image}/>;
+
+}
+
+const style = {
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  }
+}
